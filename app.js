99050b3db09ab9bb6969fe32714d6419e5c7075f
rushdi hasan <rushdi_123@hotmail.com>
2017-04-02 22:05:00 +0200

    add new file

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+
+var app = express();
+
+app.set('views', __dirname + '/view');
+
+app.set('view engine' , 'ejs');
+
+app.use('/assets',express.static('assets'));
+
+app.get('/',function(req,res){
+	res.render('index');
+});
+
+app.get('/contact',function(req,res){
+	res.render('contact',{qs:req.query});
+});
+
+app.get('/profile/:name', function(req,res){
+	var data = {age:33, job:'ninja',hobbies:['eating','fighting','fishing']};
+	res.render('profile',{person:req.params.name,data:data});
+})
+
+app.listen(3000);
+
+
+
+
